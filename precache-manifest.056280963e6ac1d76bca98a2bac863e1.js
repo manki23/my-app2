813cf1362441271591c5bd9408181fbfcaca4459
Myriam ANKI <manki@e1r3p14.42.fr>
2020-09-14 19:44:21 +0200

    Updates

---
+++ precache-manifest.056280963e6ac1d76bca98a2bac863e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff434cd4948580adad0f27fb2d588d9",
+    "url": "/my-app2/index.html"
+  },
+  {
+    "revision": "4c427a4b38baf001328f",
+    "url": "/my-app2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c000be9a922bbbae844",
+    "url": "/my-app2/static/js/2.15527987.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app2/static/js/2.15527987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c427a4b38baf001328f",
+    "url": "/my-app2/static/js/main.f9854830.chunk.js"
+  },
+  {
+    "revision": "b8ac5fe1496453d7da31",
+    "url": "/my-app2/static/js/runtime-main.234abd64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app2/static/media/logo.5d5d9eef.svg"
+  }
+]);
