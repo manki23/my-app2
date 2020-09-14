46fde09dfb3a908cd0d538f08716b0fce36f9ee8
Myriam ANKI <manki@e1r3p14.42.fr>
2020-09-14 19:55:08 +0200

    MOD: src/App.js > another small modif to test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React tesssssstttt
+          Learn React coucou
         </a>
       </header>
     </div>
